87ae63e846d3cc189aa399d655c04ca719da51ea
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2025-04-16 11:42:10 +0200

    async

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use rocksdb::DB;
+use tokio::runtime::Handle;
+use std::sync::Arc;
 
-pub fn main() {
+#[tokio::main]
+pub async fn main() {
     println!("hello");
-    let db = DB::open_default("storage_db").unwrap();
+    let db = Arc::new(DB::open_default("storage_db").unwrap());
 
     let key = [0u8; 4];
     let value = [2u8; 4];
 
     println!("First value read: {:?}", db.get(key).unwrap() );
-    db.put(key.clone(), value.clone());
-    println!("Second value read: {:?}", db.get(key).unwrap() );
+    let handle = Handle::current();
+
+    let cloned_db = db.clone();
+    handle.spawn(async move {
+        cloned_db.put(key.clone(), value.clone());
+        println!("Second value read: {:?}", db.get(key).unwrap() );
+    }).await;
 }
