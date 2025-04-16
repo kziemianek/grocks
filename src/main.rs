5bc6df95f87b84a6962b219eb9e593329dcc7d0e
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2025-04-16 14:13:46 +0200

    rpc server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use rocksdb::DB;
 use tokio::runtime::Handle;
 use std::sync::Arc;
+use jsonrpsee::server::{RpcModule, Server};
+use std::net::SocketAddr;
 
 #[tokio::main]
 pub async fn main() {
@@ -14,8 +16,23 @@ pub async fn main() {
     let handle = Handle::current();
 
     let cloned_db = db.clone();
-    handle.spawn(async move {
-        cloned_db.put(key.clone(), value.clone());
-        println!("Second value read: {:?}", db.get(key).unwrap() );
-    }).await;
+
+
+	let server = Server::builder().build("0.0.0.0:2000".parse::<SocketAddr>().unwrap()).await.unwrap();
+	let mut module = RpcModule::new(());
+	module.register_method("say_hello", move |_, _, _| {
+        cloned_db.put(key, value);
+    }).unwrap();
+
+
+    let handle = server.start(module);
+
+    tokio::spawn(handle.stopped()).await;
 }
+
+
+//     handle.spawn(async move {
+//         cloned_db.put(key.clone(), value.clone());
+//         println!("Second value read: {:?}", db.get(key).unwrap() );
+//     }).await;
+// }
